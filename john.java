7a3e68a584d1d09fb382a8953fb3ecc765c46789
John Zorrilla <johnzorrillx@gmail.com>
2023-10-27 21:49:25 -0500

    añadi yon

--- john.java
+++ john.java
@@ -2,6 +2,7 @@ public class john {
 
     public static void main(String[] args) {
             System.out.print("john");
-
+            System.out.println("yoooooooo");
+            System.out.println("seguro");
     }
 }
